4be8028ac4c537790d060ed489a9ef57a743de14
Amol Gomkale <amol.gomkale@pixonix.tech>
2023-06-05 19:03:55 +0530

    Added [ValidateJsonXssAttribute] on Add

--- VerifyWebAppUIChanges03JUN2023/VerifyWebApp/Controllers/LoginController.cs
+++ VerifyWebAppUIChanges03JUN2023/VerifyWebApp/Controllers/LoginController.cs
@@ -322,6 +322,7 @@ namespace VerifyWebApp.Controllers
         [HttpPost]
    
         [ValidateJsonAntiForgeryToken]
+        [ValidateJsonXssAttribute]
         public ActionResult Add(Login login)
         {
             int userid = 0;
