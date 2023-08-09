0f78f0742dffaee302943ccb9bc7be270eb42dcf
Mayuri <mayuri.shendre@pixonix.tech>
2023-08-09 11:21:25 +0530

    ITgroups Table displayed on page,Assembly reference ,GetAssetData method added in controller

--- VerifyWebAppUIChanges03JUN2023/VerifyWebApp/Controllers/ITGroupController.cs
+++ VerifyWebAppUIChanges03JUN2023/VerifyWebApp/Controllers/ITGroupController.cs
@@ -8,12 +8,19 @@ using VerifyWebApp.Models;
 
 using System.IO;
 using VerifyWebApp.Filter;
+using VerifyWebApp.BusinessLogic;
+using NLog;
+using Newtonsoft.Json;
 
 namespace VerifyWebApp.Controllers
 {
     public class ITGroupController : Controller
     {
         public VerifyDB db = new VerifyDB();
+
+        
+
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
 
         // GET: ITGroup
         [AuthUser]
@@ -120,6 +127,140 @@ namespace VerifyWebApp.Controllers
                 bResult = false;
             }
             return new JsonResult { Data = bResult, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        [HttpPost]
+        public ActionResult GetAssetData(string id, string searchby = "", string searchstring = "")
+        {
+
+            int userid = 0;
+            Login user = (Login)(Session["PUser"]);
+
+            if (user != null)
+            {
+                ViewBag.LogonUser = user.UserName;
+                userid = user.ID;
+            }
+            else
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            int companyid = 0;
+            Company company = (Company)(Session["Cid"]);
+
+            if (company != null)
+            {
+                ViewBag.LoggedCompany = company.CompanyName;
+                companyid = company.ID;
+                ViewBag.companyid = companyid;
+            }
+            else
+            {
+                return RedirectToAction("CompanySelection", "Company");
+            }
+            int totalResultsCount;
+            int filteredResultsCount;
+
+            List<Assets> alist = new List<Assets>();
+
+            string search = Request.Form.GetValues("search[value]")[0];
+            string draw = Request.Form.GetValues("draw")[0];
+            string order = Request.Form.GetValues("order[0][column]")[0];
+            string orderDir = Request.Form.GetValues("order[0][dir]")[0];
+            int startRec = Convert.ToInt32(Request.Form.GetValues("start")[0]);
+            int pageSize = Convert.ToInt32(Request.Form.GetValues("length")[0]);
+
+            JsonResult result = new JsonResult();
+
+            try
+            {
+                string Level = "";
+                Level = id.Substring(0, 2);
+                int tempLength = id.Length;
+                string strr_id = id.Substring(3, tempLength - 3);
+                int int_id = Convert.ToInt32(strr_id);
+
+
+                AssetRepository assetRepository = new AssetRepository();
+                if (search.Length > 0)
+                {
+                    searchstring = search;
+                }
+
+
+                if (searchstring.Length > 0)
+                {
+                    alist = assetRepository.GetAssetDataSearch(companyid, Level, int_id, startRec, pageSize, searchby, searchstring);
+
+                    int totalRecords = db.Assetss.Count(x => x.Companyid == companyid);
+                    int recFilter = totalRecords;
+
+                    var lstAssets = alist.Select(x => new
+                    {
+                        x.AssetNo,
+                        x.AssetIdentificationNo,
+                        x.AssetName,
+                        x.str_VoucherDate,
+                        x.AmountCapitalisedCompany,
+                        x.BillNo,
+                        x.Qty
+                    }).ToList();
+
+                    filteredResultsCount = lstAssets.Count;
+
+                    result = this.Json(new
+                    {
+                        draw = Convert.ToInt32(draw),
+                        recordsTotal = totalRecords,
+                        recordsFiltered = recFilter,
+                        data = alist,
+
+                    }, JsonRequestBehavior.AllowGet);
+
+                    result.MaxJsonLength = int.MaxValue;
+                }
+                else
+                {
+                    alist = assetRepository.GetAssetData(companyid, Level, int_id, startRec, pageSize);
+
+                    int totalRecords = db.Assetss.Count(x => x.Companyid == companyid);
+                    int recFilter = totalRecords;
+
+
+                    var lstAssets = alist.Select(x => new
+                    {
+                        x.AssetNo,
+                        x.AssetIdentificationNo,
+                        x.AssetName,
+                        x.str_VoucherDate,
+                        x.AmountCapitalisedCompany,
+                        x.BillNo,
+                        x.Qty
+                    }).ToList();
+
+                    filteredResultsCount = lstAssets.Count;
+
+                    result = this.Json(new
+                    {
+                        draw = Convert.ToInt32(draw),
+                        recordsTotal = totalRecords,
+                        recordsFiltered = totalRecords,
+                        data = alist,
+
+                    }, JsonRequestBehavior.AllowGet);
+
+                    result.MaxJsonLength = int.MaxValue;
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                logger.Log(LogLevel.Error, ex);
+            }
+            return result;
         }
 
         public ActionResult GetLocations()
