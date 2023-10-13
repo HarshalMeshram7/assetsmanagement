d6f77b1baf58decde7e2337dfd50701180feb385
Mayuri <mayuri.shendre@pixonix.tech>
2023-10-13 11:22:08 +0530

    ActivitylogReport excel downloaded

--- VerifyWebAppUIChanges03JUN2023/VerifyWebApp/Controllers/ActivityLogController.cs
+++ VerifyWebAppUIChanges03JUN2023/VerifyWebApp/Controllers/ActivityLogController.cs
@@ -65,12 +65,28 @@ namespace VerifyWebApp.Controllers
                 }
 
                 List<Assets> alist = new List<Assets>();
-                List<FARReportViewmodel> FARList = new List<FARReportViewmodel>();
+                List<ActivityLogViewmodel> ALogList = new List<ActivityLogViewmodel>();
 
 
-                BusinessLogic.FARReportRepository reportRepository = new BusinessLogic.FARReportRepository();
+                // BusinessLogic.FARReportRepository reportRepository = new BusinessLogic.FARReportRepository();
 
-                FARList = reportRepository.getFAR_New(companyid, FromDate, ToDate);
+                //  FARList = reportRepository.getFAR_New(companyid, FromDate, ToDate);
+
+                string strSQL = null;
+                string str_comid = companyid.ToString();
+
+                string fromDate = FromDate.ToString("yyyy-MM-dd");
+                string todate = ToDate.ToString("yyyy-MM-dd");
+
+                
+                strSQL = " SELECT `id`, `userid`, `eventid`, `recordtype`, `column`, `oldvalue`, `newvalue` FROM tblauditlog WHERE ";
+                strSQL = strSQL + " trandate >= " + "'" + fromDate + "'" + " AND trandate <= " + "'" + todate + "'";
+
+                db.Database.CommandTimeout = 180;
+
+                ALogList = db.Database.SqlQuery<ActivityLogViewmodel>(strSQL).ToList();
+
+
 
 
                 var memoryStream = new MemoryStream();
@@ -81,15 +97,10 @@ namespace VerifyWebApp.Controllers
                 {
 
                     excel.Workbook.Worksheets.Add("Worksheet1");
-                    excel.Workbook.Worksheets.Add("Worksheet2");
+                    //excel.Workbook.Worksheets.Add("Worksheet2");
 
 
-                    string[] headerRow = {"AGroupName", "BGroupName", "CGroupName ", "DGroupName","AssetNo", "AssetIdentificationNo ", "AssetName ", "VoucherDate ",
-                    "OpGross","Addition ","Disposal","ClGross","OpDep","DepForPeriod","DispoDep","TotDep","NetBalance",
-                    "ResidualVal",
-                    "VoucherNo","VoucherDate", "BillNo","PONo","BillDate","DtPutUse(Company)","DepRate","DepMethod",
-                    "Opening Qty","DisposedQtyTillFromDate", "Disposed Qty", "Closing Qty", "Product Serial No","Model","Remarks",
-                    "ALocName ","BLocName","CLocName","SupplierName",};
+                    string[] headerRow = {"UserId", "EventId", "RecordType", "TranDate", "Column", "Oldvalue", "Newvalue",};
 
 
 
@@ -104,10 +115,10 @@ namespace VerifyWebApp.Controllers
 
 
                     var worksheet = excel.Workbook.Worksheets["Worksheet1"];
-                    var worksheet2 = excel.Workbook.Worksheets["Worksheet2"];
+                 //   var worksheet2 = excel.Workbook.Worksheets["Worksheet2"];
                     var currentSheet = excel.Workbook.Worksheets;
                     // Popular header row data
-                    worksheet.Cells[1, 1].Value = "Fixed Asset Register Report";
+                    worksheet.Cells[1, 1].Value = "ActivityLog Report";
                     worksheet.Cells[2, 2].Value = "FromDate:  " + FromDate.ToString("dd/MM/yyyy") + " ToDate:" + ToDate.ToString("dd/MM/yyyy");
 
                     int col = 1;
@@ -123,110 +134,24 @@ namespace VerifyWebApp.Controllers
 
 
 
-                    foreach (var item in FARList)
+                    foreach (var item in ALogList)
                     {
-                        worksheet.Cells[rowIterator, 1].Value = item.AGroupName;
-                        worksheet.Cells[rowIterator, 2].Value = item.BGroupName;
-                        worksheet.Cells[rowIterator, 3].Value = item.CGroupName;
-                        worksheet.Cells[rowIterator, 4].Value = item.DGroupName;
+                        worksheet.Cells[rowIterator, 1].Value = item.UserId;
+                        worksheet.Cells[rowIterator, 2].Value = item.RecordType;
+                        worksheet.Cells[rowIterator, 3].Value = item.EventId;
+                        worksheet.Cells[rowIterator, 4].Value = item.TranDate;
 
-                        worksheet.Cells[rowIterator, 5].Value = item.AssetNo;
-                        worksheet.Cells[rowIterator, 6].Value = item.AssetIdentificationNo;
-                        worksheet.Cells[rowIterator, 7].Value = item.AssetName;
-                        worksheet.Cells[rowIterator, 8].Value = item.str_voucherdate;
-                        worksheet.Cells[rowIterator, 9].Value = item.OpGross;
-                        worksheet.Cells[rowIterator, 10].Value = item.Addition;
-                        worksheet.Cells[rowIterator, 11].Value = item.Disposal;
-                        worksheet.Cells[rowIterator, 12].Value = item.ClGross;
-                        worksheet.Cells[rowIterator, 13].Value = item.OpDep;
-
-                        worksheet.Cells[rowIterator, 14].Value = item.UpToDep;
-                        worksheet.Cells[rowIterator, 15].Value = item.DispoDep;
-                        worksheet.Cells[rowIterator, 16].Value = item.TotDep;
-                        worksheet.Cells[rowIterator, 17].Value = item.NetBalance;
-
-
-
-                        worksheet.Cells[rowIterator, 18].Value = item.ResidualVal;
-
-
-                        worksheet.Cells[rowIterator, 19].Value = item.VoucherNo;
-
-
-
-                        if (item.voucherDate.HasValue)
-                        {
-                            DateTime dtVouucherDate = item.voucherDate.Value.Date;
-
-                            worksheet.Cells[rowIterator, 20].Value = dtVouucherDate.ToString("dd/MM/yyyy");
-                            // worksheet.Cells[rowIterator, 19].Style.Numberformat.Format = "dd/mm/yyyy";
-
-                        }
-                        else
-                        {
-                            worksheet.Cells[rowIterator, 20].Value = "";
-                        }
-
-
-                        worksheet.Cells[rowIterator, 21].Value = item.BillNo;
-                        worksheet.Cells[rowIterator, 22].Value = item.PONo;
-
-
-                        if (item.BillDate.HasValue)
-                        {
-                            DateTime dtBillDate = item.BillDate.Value.Date;
-                            worksheet.Cells[rowIterator, 23].Value = dtBillDate.Date.ToString("dd/MM/yyyy");
-
-                        }
-                        else
-                        {
-                            worksheet.Cells[rowIterator, 23].Value = "";
-                        }
-
-
-
-
-
-                        if (item.DTPutUse.HasValue)
-                        {
-                            DateTime dtDTPutUse = item.DTPutUse.Value.Date;
-                            worksheet.Cells[rowIterator, 24].Value = dtDTPutUse.ToString("dd/MM/yyyy");
-                            // worksheet.Cells[rowIterator, 23].Style.Numberformat.Format = "dd/mm/yyyy";
-                        }
-                        else
-                        {
-                            worksheet.Cells[rowIterator, 24].Value = "";
-                        }
-
-                        worksheet.Cells[rowIterator, 25].Value = item.DepRate;
-                        worksheet.Cells[rowIterator, 26].Value = item.DepMethod;
-
-                        worksheet.Cells[rowIterator, 27].Value = item.OpeningQty;
-                        worksheet.Cells[rowIterator, 28].Value = item.DisposedQtyTillFromDate;
-                        worksheet.Cells[rowIterator, 29].Value = item.DisposedQty;
-                        worksheet.Cells[rowIterator, 30].Value = item.ClosingQty;
-                        worksheet.Cells[rowIterator, 31].Value = item.SrNo;
-
-
-                        //worksheet.Cells[rowIterator, 32].Value = item.SrNo;
-
-                        worksheet.Cells[rowIterator, 32].Value = item.Model;
-
-                        worksheet.Cells[rowIterator, 33].Value = item.Remarks;
-                        worksheet.Cells[rowIterator, 34].Value = item.ALocName;
-                        worksheet.Cells[rowIterator, 35].Value = item.BLocName;
-                        worksheet.Cells[rowIterator, 36].Value = item.CLocName;
-                        worksheet.Cells[rowIterator, 37].Value = item.SupplierName;
-
-
-
+                        worksheet.Cells[rowIterator, 5].Value = item.Column;
+                        worksheet.Cells[rowIterator, 6].Value = item.Oldvalue;
+                        worksheet.Cells[rowIterator, 7].Value = item.Newvalue;
+                      
 
                         rowIterator = rowIterator + 1;
 
                     }
 
 
-                    string excelName = "FARReport.xlsx";
+                    string excelName = "ActivityLogReport.xlsx";
 
                     string handle = Guid.NewGuid().ToString();
                     excel.SaveAs(memoryStream);
---
+++ VerifyWebAppUIChanges03JUN2023/VerifyWebApp/ViewModel/ActivityLogViewmodel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VerifyWebApp.ViewModel
+{
+    public class ActivityLogViewmodel
+    {
+
+        public int UserId { get; set; }
+        public int EventId { get; set; }
+        public int RecordType { get; set; }
+        public DateTime TranDate { get; set; }
+        public string Column { get; set; }
+        public string Oldvalue { get; set; }
+        public string Newvalue { get; set; }
+
+    }
+}
